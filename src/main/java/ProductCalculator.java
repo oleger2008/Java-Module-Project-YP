c12b5aa1f8b5e60eca88dd59256ce65c104ab28b
oleger2008 <otsoy1995@yandex.ru>
2022-12-22 21:00:41 +0300

    edit storing products
    don't have to store products at all. can store only their names.

--- src/main/java/ProductCalculator.java
+++ src/main/java/ProductCalculator.java
@@ -2,16 +2,16 @@ import java.util.Vector;
 
 public class ProductCalculator {
     private double totalCost_ = 0.;
-    private Vector<Product> products_ = new Vector<>();
+    private Vector<String> products_ = new Vector<>();
 
     public void addProduct(Product product) {
-        products_.add(product);
+        products_.add(product.getName());
         totalCost_ += product.getPrice();
     }
 
     public void printAllProducts() {
-        for (Product product : products_) {
-            System.out.println(product.getName());
+        for (String product : products_) {
+            System.out.println(product);
         }
     }
 
