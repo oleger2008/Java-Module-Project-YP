50ef39f13afb1b26b54a367446208a6fd4f8a5db
oleger2008 <otsoy1995@yandex.ru>
2022-12-22 19:33:20 +0300

    add code for whole task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Введи на сколько человек нужно разделить счёт:");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите на сколько человек нужно разделить счёт:");
+        int peopleAmount = readPeopleAmount();
+
+        ProductCalculator calculator = new ProductCalculator();
+        String command = "";
+        final String endCommand = "завершить";
+        while (!endCommand.equalsIgnoreCase(command)) {
+            calculator.addProduct(readProduct());
+            System.out.println("Напишите слово \"Завершить\" для завершения добавления товаров");
+            command = scanner.next();
+        }
+
+        System.out.println("Добавленные товары:");
+        calculator.printAllProducts();
+        printAveragePrice(calculator.getTotalCost(), peopleAmount);
+    }
+
+    public static int readPeopleAmount() {
         int peopleAmount = scanner.nextInt();
         while (peopleAmount < 1) {
             System.out.println("Введите корректное количество человек:");
             peopleAmount = scanner.nextInt();
         }
+        return peopleAmount;
+    }
+
+    public static Product readProduct() {
+        System.out.println("Введите название товара, который хотите добавить:");
+        String productName = scanner.next();
+        System.out.println("Введите стоимость этого товара:");
+        double price = scanner.nextDouble();
+        while (price < 0.) {
+            System.out.println("Введите корректное значение цены товара: " + productName);
+            price = scanner.nextDouble();
+        }
+        return new Product(productName, price);
+    }
+
+    public static void printAveragePrice(double totalCost, int peopleAmount) {
+        double avgPrice = totalCost / peopleAmount;
+        System.out.println("Каждый должен заплатить по:");
+        String rubleWord = (Math.floor(avgPrice) == 1.) ? "рубль" : "рубля";
+        System.out.println(String.format("%.2f %s", avgPrice, rubleWord));
     }
 }
